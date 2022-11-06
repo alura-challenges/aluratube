e367be1344310b2d000ee2fb781979a60ee30fcb
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-11-06 07:21:49 -0800

    a1: final

---
+++ src/components/CSSReset.js
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from "styled-components";
+
+export const CSSReset = createGlobalStyle`
+  /* Reset */
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-family: sans-serif;
+  }
+  /* NextJS */
+  html {
+    display: flex;
+    flex-direction: column;
+    min-height: 100%;
+  }
+  body {
+    display: flex;
+    flex: 1;
+  }
+  #__next {
+    display: flex;
+    flex: 1;
+  }
+  /* Globals */
+  button,
+  a {
+    text-decoration: none;
+    opacity: 1;
+    transition: .3s;
+    &:hover,
+    &:focus {
+      opacity: .5;
+    }
+  }
+`;
---
+++ src/components/Timeline.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const StyledTimeline = styled.div`
+  flex: 1;
+  width: 100%;
+  padding: 16px;
+  overflow: hidden;
+  h2 {
+    font-size: 16px;
+    margin-bottom: 16px;
+    text-transform: capitalize;
+  }
+  img {
+    aspect-ratio: 16/9;
+    font-weight: 500;
+    object-fit: cover;
+    width: 100%;
+    max-width: 210px;
+    height: auto;
+  }
+  section {
+    width: 100%;
+    padding: 0;
+    overflow: hidden;
+    padding: 16px;
+    div {
+      
+      width: calc(100vw - 16px * 4);
+      display: grid;
+      grid-gap: 16px;
+      grid-template-columns: repeat(auto-fill,minmax(200px,1fr));
+      grid-auto-flow: column;
+      grid-auto-columns: minmax(200px,1fr);
+      overflow-x: scroll;
+      scroll-snap-type: x mandatory;
+      a {
+        scroll-snap-align: start;
+        span {
+          padding-top: 8px;
+          display: block;
+          padding-right: 24px;
+          color: ${({ theme }) => theme.textColorBase || "#222222"};
+        }
+      }
+    }
+  }
+`;
